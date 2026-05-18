31330bcde32882755842acce0ed049acc0ffaeab
Oussama Ezahidi <oussamaezahidi@gmail.com>
2026-05-18 17:40:10 +0100

    Build and serve React frontend from Laravel for production deployment

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,9 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return file_exists(public_path('index.html'))
+        ? file_get_contents(public_path('index.html'))
+        : view('welcome');
 });
 
 Route::fallback(function () {
-    return view('welcome');
+    return file_exists(public_path('index.html'))
+        ? file_get_contents(public_path('index.html'))
+        : view('welcome');
 });
